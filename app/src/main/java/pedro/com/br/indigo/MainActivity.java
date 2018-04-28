5d4fdd536b5f89d108b1e55d18d4230875fc2c85
sedordiga <sedordiga@gmail.com>
2018-04-27 23:22:30 -0300

    logout

--- app/src/main/java/pedro/com/br/indigo/MainActivity.java
+++ app/src/main/java/pedro/com/br/indigo/MainActivity.java
@@ -3,6 +3,9 @@ package pedro.com.br.indigo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -10,7 +13,7 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
-
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mAuth = FirebaseAuth.getInstance();
-
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Indigo");
     }
 
     @Override
@@ -29,9 +34,39 @@ public class MainActivity extends AppCompatActivity {
 
 
         if(currentUser == null){
-            Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startIntent);
-            finish();
+            sendToStart();
         }
+    }
+
+    private void sendToStart(){
+
+        Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        super.onCreateOptionsMenu(menu);
+
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        super.onOptionsItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_btn){
+            FirebaseAuth.getInstance().signOut();
+            sendToStart();
+
+        }
+
+        return true;
     }
 }
