da1a0a526d00a190b76de232cd7c68c219073218
Bruno Dantas <brunoodantas7@gmail.com>
2018-05-29 03:17:20 -0300

    Finalizada construcao de perfil

--- app/src/main/java/pedro/com/br/indigo/ProfileActivity.java
+++ app/src/main/java/pedro/com/br/indigo/ProfileActivity.java
@@ -2,11 +2,19 @@ package pedro.com.br.indigo;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,15 +29,18 @@ public class ProfileActivity extends AppCompatActivity {
     private Button mProfileSendReqBtn;
 
     private DatabaseReference mUserDataBase;
-
+    private DatabaseReference mFriendRequestDataBase;
+    private FirebaseUser mCurrent_user;
     private ProgressDialog mProgressDialog;
+
+    private String mCurrent_state;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        String user_id = getIntent().getStringExtra("user_id");
+        final String user_id = getIntent().getStringExtra("user_id");
 
         mUserDataBase = FirebaseDatabase.getInstance().getReference().child("Users").child(user_id);
 
@@ -39,6 +50,12 @@ public class ProfileActivity extends AppCompatActivity {
         mProfileFriendsCount = (TextView) findViewById(R.id.profile_friends_count);
         mProfileSendReqBtn = (Button) findViewById(R.id.profile_btn_send_req);
         mProfileImage = (ImageView) findViewById(R.id.profile_image);
+
+        mCurrent_state = "not_friends";
+        mFriendRequestDataBase = FirebaseDatabase.getInstance().getReference().child("Friend_Request");
+
+        //CAPTURANDO USUARIO ATUAL
+        mCurrent_user = FirebaseAuth.getInstance().getCurrentUser();
 
         //POP UP DE CARREGAMENTO DOS DADOS
         mProgressDialog = new ProgressDialog(ProfileActivity.this);
@@ -72,6 +89,53 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
 
+        mProfileSendReqBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(mCurrent_state.equals("not_friends")){
+
+                    //CRIA A REFERENCIA DA SOLICITACAO DE AMIZADE NO BANCO E CAPTURA O 'ID' DO USUARIO QUE A FEZ A SOLICITACAO
+                    // EM SEGUIDA SETA O TIPO DE REQUISIÇÃO (ENVIADO) E O ID DO USUARIO QUE RECEBEU A SOLICITACAO.
+                    mFriendRequestDataBase.child(mCurrent_user.getUid()).child(user_id).child("request_type")
+                            .setValue("sent").addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful()){
+
+                                //CAPTURA O ID DO USUARIO QUE FOI ALVO DA SOLICITACAO DE AMIZADE E SETA ISSO NO BANCO
+                                //EM SEGUIDA SETA O TIPO DE REQUISICAO (RECEBIDO) E O USUARIO(ID) DE ORIGEM QUE FEZ A SOLICITACAO DE AMIZADE
+                                mFriendRequestDataBase.child(user_id).child(mCurrent_user.getUid()).child("request_type")
+                                        .setValue("received").addOnSuccessListener(new OnSuccessListener<Void>() {
+
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+
+                                        Toast.makeText(ProfileActivity.this, "Solicitação Enviada", Toast.LENGTH_SHORT).show();
+
+
+                                    }
+                                });
+
+                            }
+                            else{
+
+                                Toast.makeText(ProfileActivity.this, "Falha no envio da solicitação", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+                    });
+
+                }
+                else{
+
+                }
+
+            }
+        });
 
     }
 }
